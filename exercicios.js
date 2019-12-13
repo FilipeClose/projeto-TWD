0eff0489e423f494623e061565fbb7896790e8cb
Filipe <filipepanhota@gmail.com>
2019-12-13 19:44:56 -0300

    Segundo commit. Criação do primeiro arquivo de exercicios

---
+++ exercicios.js
@@ -0,0 +1,22 @@
+console.log(typeof 'Joao');
+console.log(typeof 18);
+console.log(typeof true);
+console.log(typeof [10, 20, 30]);
+
+var pessoa = {
+    nome: "Filipe",
+    idade: 18,
+    sexo: "Masculino"
+}
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.sexo);
+
+function soma(x, y) {
+    var resultado = x + y;
+    console.log(resultado)
+
+}
+
+soma(10, 20);
